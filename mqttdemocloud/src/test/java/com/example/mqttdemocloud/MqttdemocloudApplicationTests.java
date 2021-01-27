37caf307e7a1ded70554d4a7b38cb02345fbbe7c
Shantanu Singh Jhala <ssjhala91@gmail.com>
2021-01-27 11:18:54 +0530

    Add files via upload

---
+++ mqttdemocloud/src/test/java/com/example/mqttdemocloud/MqttdemocloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqttdemocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttdemocloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
